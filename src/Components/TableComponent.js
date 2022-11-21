ba011c70873ab9a5c56288cab75008196847debf
TetianaKosynets <kch.tetiana@gmail.com>
2022-11-21 15:19:40 +1100

    add new row

--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -15,36 +15,36 @@ export default function TableComponent(props) {
       };
 
       const addClick = () => {
-
+        const rowsInput= rowsData[0];
+        setRowsData([...rowsData, rowsInput])
       };
 
     return (
         <div>
-        <button onClick={loadClick}>Load</button>
-        <button onClick={deleteClick}>Delete</button>
-        <button onClick={addClick}>Add</button>
+            <button onClick={loadClick}>Load</button>
+            <button onClick={deleteClick}>Delete</button>
+            <button onClick={addClick}>Add</button>
   
-        <table>
-  <tr>
-    <th>Country</th>
-    <th>Domains</th>
-    <th>Web Pages</th>
-    <th>Alpha Two Code</th>
-    <th>Name</th>
-    <th>State-province</th>
-  </tr>
-  {rowsData.map(item =>
-  <tr>
-  <td>{item.name}</td>
-  <td>{item.domains}</td>
-  <td>{item.web_pages}</td>
-  <td>{item.alpha_two_code}</td>
-  <td>{item.name}</td>
-  <td>{item.state}</td>
-</tr>
-                    )}
-
-</table>
+            <table>
+                <tr>
+                    <th>Country</th>
+                    <th>Domains</th>
+                    <th>Web Pages</th>
+                    <th>Alpha Two Code</th>
+                    <th>Name</th>
+                    <th>State-province</th>
+                </tr>
+                {rowsData.map(item =>
+                    <tr>
+                        <td>{item.name}</td>
+                        <td>{item.domains}</td>
+                        <td>{item.web_pages}</td>
+                        <td>{item.alpha_two_code}</td>
+                        <td>{item.name}</td>
+                        <td>{item.state}</td>
+                    </tr>
+                )}
+            </table>
             </div>
         );           
     }
